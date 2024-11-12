9d6e3264c6c7c3c3484036f6bc49cb232399abe6
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 20:53:12 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,10 @@
+"use strict";
+//10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//My Name:Yusra Imran
+//Current Date: 9/feb/2024
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is equal to ${sum}`);
+//in this program we use let to declare the variables num1,num2 and sum
+//then calculating the sum of num1 and num2 and displaying the result on the terminal.
